a553a2eb0dcbd00f393cfcfdc5ea0a89347caec4
dao hodac <dao.hodac@gmail.com>
2017-09-27 16:10:23 +0200

    adds the browser to the spreadsheet

--- index.py
+++ index.py
@@ -3,6 +3,8 @@ import SocketServer
 import requests
 from datetime import datetime
 import os
+import logging
+
 redir = os.environ['REDIRECT']
 zapier= os.environ['ZAPIER_HOOK']
 
@@ -11,8 +13,8 @@ class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
    def do_GET(self):
        print self.path
        now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
-       requests.post(zapier, json={"IP":self.client_address[0], "date": now, "type": self.path})
-       self.send_response(303)
+       requests.post(zapier, json={"IP":self.client_address[0], "date": now, "type": self.path, "browser":self.headers.get('User-Agent')})
+       self.send_response(302)
        #new_path = '%s%s'%(redir, self.path)
        new_path = redir
        self.send_header('Location', new_path)
